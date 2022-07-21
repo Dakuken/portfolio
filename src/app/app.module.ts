edc35620ce416d933cc3b6271659574347347a20
BURNEL Justin <justin.burnel3@etu.univ-lorraine.fr>
2022-07-21 14:12:52 +0200

    normalement tout works

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,18 +6,30 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NbThemeModule, NbLayoutModule } from '@nebular/theme';
 import { NbEvaIconsModule } from '@nebular/eva-icons';
+import { HomeComponent } from './home/home.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { HomeModule } from './home/home.module';
+import { HeaderModule } from './header/header.module';
+import { FooterModule } from './footer/footer.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
+
+    HomeModule,
+    HeaderModule,
+    FooterModule,
+
     NbThemeModule.forRoot({ name: 'cosmic' }),
     NbLayoutModule,
     NbEvaIconsModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
