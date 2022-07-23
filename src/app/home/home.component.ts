1a47617cdb5d30140c78b21caaa57a71503ab913
BURNEL Justin <justin.burnel3@etu.univ-lorraine.fr>
2022-07-23 20:53:00 +0200

    ajout de style sur l'accueil

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,9 +1,24 @@
 import { Component, OnInit } from '@angular/core';
+import {
+  trigger,
+  state,
+  style,
+  animate,
+  transition,
+  // etc...
+} from '@angular/animations';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
+  animations: [trigger('fade', [
+    state('inactive', style({ opacity: 0 })),
+    state('active', style({ opacity: 1 })),
+    transition('* <=> *', [
+      animate(2000)
+    ])
+  ]),]
 })
 export class HomeComponent implements OnInit {
 
@@ -11,5 +26,7 @@ export class HomeComponent implements OnInit {
 
   ngOnInit(): void {
   }
+
+
 
 }
--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeComponent } from './home.component';
+import { NbCardModule } from '@nebular/theme';
 
 
 
@@ -9,7 +10,8 @@ import { HomeComponent } from './home.component';
     HomeComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    NbCardModule,
   ]
 })
 export class HomeModule { }
