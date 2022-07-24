53580ffaa6d2009684aa5d95316a39a47137b722
BURNEL Justin <justin.burnel3@etu.univ-lorraine.fr>
2022-07-24 07:55:24 +0200

    ok je suis satisfait des animations

--- src/app/route-animation.ts
+++ src/app/route-animation.ts
@@ -11,28 +11,24 @@ import {
 
 export const slider =
     trigger('routeAnimations', [
-        transition('* => Accueil', [query('.machin', animTo(0))]),
-        transition('* => isRight', slideTo('right')),
-        transition('isRight => *', slideTo('left')),
+        transition('Accueil => *', fade('right')),
         // transition('Accueil => *', [query('.machin', animTo(1))]),
     ])
 
-function slideTo(direction: string) {
+function fade(direction: string) {
     const optional = { optional: true }
     return [
-        query(':enter, :leave', [
+        query(' :leave', [
             style({
                 position: 'absolute',
                 top: 0,
                 [direction]: 0,
-                with: '100%'
+                with: '100%',
+                overflow: 'hidden',
+                opacity: 1,
+
             })], optional),
-        query(':enter', [style({ [direction]: '-100%' })]),
-        group([
-            query(':leave', [animate('600ms ease', style({ [direction]: '100%' }))], { optional: true }),
-            query(':enter', [animate('600ms ease', style({ [direction]: '0%' }))
-            ]),
-        ])
+        query(':leave', [animate('300ms ease', style({ top: '50%', [direction]: '50%', opacity: 0, overflow: 'hidden', }))], { optional: true }),
     ]
 }
 
