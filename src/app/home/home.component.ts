5f0e4f39ef49475ab71a9011c6c6e9b8154369ac
BURNEL Justin <justin.burnel3@etu.univ-lorraine.fr>
2022-07-24 04:00:29 +0200

    on clean un peu

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,57 +1,19 @@
-import { trigger } from '@angular/animations';
 import { Component, OnInit } from '@angular/core';
 import { anim } from '../route-animation';
-import {
-
-  transition,
-  style,
-  query,
-  group,
-  animateChild,
-  animate,
-  keyframes
-} from '@angular/animations';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss'],
-  animations: [anim,
-
-    trigger('fadeSlideInOut', [
-      transition(':enter', [
-        style({ opacity: 0, transform: 'translateY(10px)' }),
-        animate('500ms', style({ opacity: 1, transform: 'translateY(0)' })),
-      ]),
-      transition(':leave', [
-        animate('500ms', style({ opacity: 0, transform: 'translateY(10px)' })),
-      ]),
-    ])]
+  animations: [anim]
 })
 
 
 export class HomeComponent implements OnInit {
 
-  visible: boolean = false
-
   constructor() {
-
   }
 
   ngOnInit(): void {
   }
-
-
-  onShow() {
-    console.log("visible", this.visible);
-
-    this.visible = !this.visible
-  }
-
-
-}
-
-
-function pouet(po: boolean) {
-
 }
--- src/app/route-animation.ts
+++ src/app/route-animation.ts
@@ -4,33 +4,9 @@ import {
     style,
     query,
     group,
-    animateChild,
     animate,
-    keyframes,
     stagger
 } from '@angular/animations';
-
-
-//baisc
-
-// export const fader =
-//     trigger('routeAnimations', [
-//         transition('* <=> *', [
-//             query(':enter, :leave', [
-//                 style({
-//                     position: 'absolute',
-//                     left: 0,
-//                     width: '100%',
-//                     opacity: 0,
-//                     transform: 'scale(0) translateY(100%)',
-//                 })
-//             ]),
-//             query(':enter', [
-//                 animate('600ms ease',
-//                     style({ opacity: 1, transform: 'scale(1)' }))
-//             ])
-//         ])
-//     ])
 
 export const slider =
     trigger('routeAnimations', [
